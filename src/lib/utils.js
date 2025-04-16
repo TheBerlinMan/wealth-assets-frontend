a86c05cdecf31a8dade9f9102c4d8a3a3de893cd
Berlin <tommyonik@gmail.com>
2025-04-16 19:13:23 -0400

    add curreny format function

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -4,3 +4,10 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs) {
   return twMerge(clsx(inputs));
 }
+
+export function formatCurrency(value) {
+  return new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+  }).format(value);
+}
