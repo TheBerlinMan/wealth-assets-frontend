cdb1f7d25dc95e8d80e83dfb1a21ba60200974b2
Berlin <tommyonik@gmail.com>
2025-04-16 19:13:34 -0400

    style landing

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
-import * as AssetService from './Services/AssetService'
-import { useEffect, useState } from 'react'
+import { useEffect, useState } from "react";
+
+import * as AssetService from "./Services/AssetService";
+
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { formatCurrency } from "./lib/utils";
 
 function App() {
-  const [assets, setAssets] = useState([])
+  const [assets, setAssets] = useState([]);
 
   useEffect(() => {
     const fetchAssets = async () => {
@@ -13,13 +17,26 @@ function App() {
     fetchAssets();
   }, []);
 
-  console.log(assets)
+  console.log(assets);
 
+  const totalAssetBalance = assets.reduce(
+    (acc, asset) => acc + asset.balanceCurrent, 0
+  );
+
+  if (assets.length === 0) return <div>Loading...</div>;
   return (
-    <>
-      <h1>Hello World</h1>
-    </>
-  )
+    <div className="text-white m-6 pt-2 space-y-4 ">
+      <h1 className="text-2xl font-bold">Account Dashboard</h1>
+      <Card>
+        <CardContent>
+          <div className="flex justify-between text-lg font-bold">
+            <h1>Total Assets</h1>
+            <h1>{formatCurrency(totalAssetBalance)}</h1>
+          </div>
+        </CardContent>
+      </Card>
+    </div>
+  );
 }
 
-export default App
+export default App;
