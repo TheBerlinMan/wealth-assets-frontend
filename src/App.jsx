ad4b929387bbae81505b2fc4ac983c6f2ea7ec24
Berlin <tommyonik@gmail.com>
2025-04-16 19:38:55 -0400

    display assets overview on landing

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 
 import * as AssetService from "./Services/AssetService";
+import AssetOverview from "./components/AssetsOverview/AssetsOverview";
 
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { formatCurrency } from "./lib/utils";
@@ -33,6 +34,19 @@ function App() {
             <h1>Total Assets</h1>
             <h1>{formatCurrency(totalAssetBalance)}</h1>
           </div>
+        </CardContent>
+      </Card>
+      <Card>
+        <CardHeader>
+          <CardTitle>
+            <h1 className="text-lg font-bold">Assets by Category</h1>
+            <h1 className="text-sm font-bold text-gray-500 -mb-2">
+              Category / Subcategory / Asset
+            </h1>
+          </CardTitle>
+        </CardHeader>
+        <CardContent>
+          <AssetOverview assets={assets} />
         </CardContent>
       </Card>
     </div>
